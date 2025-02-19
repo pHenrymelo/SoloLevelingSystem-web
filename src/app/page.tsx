1322b8181a097bbe67b95ba6468e5a70e1a9fa11
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-02-19 00:33:55 -0300

    configuration and variables setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <div className="min-h-dvh flex flex-col justify-center gap-16 fmd:flex-row bg-blue-dark ">
+      <div className=" mx-auto border border-white p-4 rounded-xs shadow-activated">
+
+      <h1 className=" text-6xl drop-shadow-sm font-bold tracking-widest">
+        ARISE
+      </h1>
+
+      </div>
+    </div>
   );
 }
